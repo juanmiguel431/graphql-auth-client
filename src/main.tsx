2b12acbbdc003de3ebb56ab4c7d6c8c490a86bf0
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-27 07:28:48 -0500

    Materialize installed and Apollo and React router wired up

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App.tsx'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <RouterProvider router={router}/>
+    </ApolloProvider>
   </React.StrictMode>,
-)
+);
