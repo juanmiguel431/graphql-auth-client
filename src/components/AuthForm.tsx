f7b93a686feb6153f436c7a4b9ec6a251d46e094
juanmiguel431 <juanmiguel431@gmail.com>
2023-11-28 19:10:58 -0500

    layout style added to Authform

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -21,34 +21,36 @@ const AuthForm: React.FC<AuthFormProps> = ({ onSubmit, errors }) => {
   }, [onSubmit, email, password]);
 
   return (
-    <form onSubmit={_onSubmit}>
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        id="email"
-        placeholder="someone@domain.com"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-      />
+    <div className="row">
+      <form onSubmit={_onSubmit} className="col s6">
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          id="email"
+          placeholder="someone@domain.com"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
 
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        placeholder="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-      />
-      {errors && errors.length > 0 &&
-        <div className={classes.errors}>
-          {errors.map(e => (
-            <div key={e}>{e}</div>
-          ))}
-        </div>
-      }
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        {errors && errors.length > 0 &&
+          <div className={classes.errors}>
+            {errors.map(e => (
+              <div key={e}>{e}</div>
+            ))}
+          </div>
+        }
 
-      <button>Submit</button>
-    </form>
+        <button className="btn waves-effect waves-ligh">Submit</button>
+      </form>
+    </div>
   );
 };
 
